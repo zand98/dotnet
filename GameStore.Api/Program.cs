347ab5b3c2bcb55e56dc346abfcf5b1c2be4e4e6
zand98 <zand.kurdy@gmail.com>
2025-07-24 15:42:55 +0300

    add program.cs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,18 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!"); 
+List<GameDto> games = [
+ new(1, "Street Fighter I", "Fighting", 19.99M, new DateOnly (1992, 7, 15)),
+ new(2, "Final Fantasy", "RolePlaying", 59.99M, new DateOnly (2010, 9, 30)),
+];
+
+app.MapGet("games", () => games);
+
+// app.MapGet("games/{id}",(int id)=> )
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
