9a892896b3e7e17ab5c522e969c2ddaa621b0687
zand98 <zandyasin98@gmail.com>
2025-08-10 10:16:55 +0300

    configure routes + validation + database + migration + entity

---
+++ GameStore.Api/Data/Migrations/20250805075934_AddnewRecordToGenreAsa.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddnewRecordToGenreAsa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 6, "new Kids and family" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
